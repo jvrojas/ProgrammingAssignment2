98890469c67af00f07b53d72dce71e40dc54b28a
João Victor Rojas Luiz <52331861+jvrojas@users.noreply.github.com>
2020-07-12 11:57:58 -0300

    Complete assignment
    This is my solution to the Programming Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions that are used to create a special object 
+## that stores a matrix and cache's its inverse.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that caches 
+## the inverse of a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the
+## matrix has not changed), then the cacheSolve retrieves the inverse from 
+## the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
+
+## This is a test of the two functions created above, from a matrix created
+## as an example.
+mat1<-c(1,2,3)
+mat2<-c(6,5,6)
+mat3<-c(7,8,9)
+matr<-cbind(mat1, mat2, mat3)
+matr
+
+specialmatr <- makeCacheMatrix(matr)
+cacheSolve(specialmatr)
+
